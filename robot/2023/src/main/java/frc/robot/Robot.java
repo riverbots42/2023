e22d4c6726af54cacf0865a898b517e5bb14b8f3
Dante <Dante.rockwell.100000000000@gmail.com>
2023-01-12 16:01:07 -0800

    Fixed it.

--- robot/2023/src/main/java/frc/robot/Robot.java
+++ robot/2023/src/main/java/frc/robot/Robot.java
@@ -82,9 +82,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    System.out.println("hi there");
+    System.out.println("hello there.");
   }
-
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
