980c89f2419667966ea0a35004bc59e335d2958c
perhapsAmeliorated <amelioratedMail@gmail.com>
2023-01-20 15:48:58 -0800

    UnfinishedMotorControls

--- robot/2023/src/main/java/frc/robot/Robot.java
+++ robot/2023/src/main/java/frc/robot/Robot.java
@@ -100,7 +100,6 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    System.out.println("Hola allí.");
   }
   @Override
   public void testInit() {
