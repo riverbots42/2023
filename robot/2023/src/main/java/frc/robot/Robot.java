1d37f2adebb705e33da593bec7f0188644f12257
perhapsAmeliorated <amelioratedMail@gmail.com>
2023-02-17 17:04:19 -0800

    Non-Functional Autonomous Files Added

--- robot/2023/src/main/java/frc/robot/Robot.java
+++ robot/2023/src/main/java/frc/robot/Robot.java
@@ -24,7 +24,6 @@ import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
